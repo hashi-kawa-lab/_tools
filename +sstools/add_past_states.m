974b1d68ec96059041aef9d4832245e5a07984f6
Takahiro Kawaguchi <kawaguchi@gunma-u.ac.jp>
2025-01-05 19:17:51 +0900

    可観測・不可観測部分を抽出する行列を追加

--- +sstools/add_past_states.m
+++ +sstools/add_past_states.m
@@ -1,4 +1,4 @@
-function sys_out = add_past_states(sys, n, C)
+function [sys_out, T1, T2] = add_past_states(sys, n, C)
 
 if nargin < 3
     C = eye(order(sys));
@@ -11,6 +11,9 @@ Anew = [Anew,  zeros(size(Anew, 1), size(C, 1))];
 Cnew = [sys.C, zeros(size(sys.c, 1), n*size(C, 1))];
 Bnew = [sys.b; zeros(size(A_add, 1), size(sys.b, 2))];
 sys_out = ss(Anew, Bnew, Cnew, sys.d, sys.Ts);
+
+T1 = tools.add_zeros(eye(order(sys)), 2, like=A_add, position="end");
+T2 = tools.add_zeros(eye(size(A_add, 1)), 2, like=sys.a, position="start");
 
 end
 
